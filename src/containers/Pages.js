1b044b5fb0b374465907703c4baba901f412d84a
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-05-26 20:37:19 +0200

    faker added

--- src/containers/Pages.js
+++ src/containers/Pages.js
@@ -1,28 +1,28 @@
 import React, {Component} from 'react';
 import Gallery from '../components/Gallery/Gallery';
+import Api from './fakeApi';
 
 class Pages extends Component {
     constructor(props){
         super(props);
-        this.state = {persons:
-            [
-                {photo:'./images/mustache1.jpg', id: '11'},
-                {photo:'./images/mustache2.jpg', id: '12'},
-                {photo:'./images/mustache3.jpg', id: '13'},
-                {photo:'./images/mustache4.jpg', id: '14'},
-                {photo:'./images/mustache5.jpg', id: '15'},
-                {photo:'./images/mustache6.jpg', id: '16'},
-                {photo:'./images/mustache7.jpg', id: '17'},
-                {photo:'./images/mustache8.jpg', id: '18'},
-                {photo:'./images/mustache9.jpg', id: '19'}
-            ]
-        };
-    }
+        this.state ={persons:[]};
+    };
+       
+    
+    setStateHandler ()  {
+       const api = new Api();
+       const persons = api.persons;
+        this.setState({
+            persons:  [...persons]
+        })
+    };
 
     render(){
-       return(
-           <Gallery list={this.state.persons}/>
+        window.addEventListener('load', this.setStateHandler.bind(this));
+        return(
+           <Gallery  list={this.state.persons}/>
        )
     }
 };
 export default Pages;
+
---
+++ src/containers/fakeApi.js
@@ -0,0 +1,28 @@
+class Api {
+    constructor(){
+        this.persons =
+                    [
+                        {photo:'./images/mustache1.jpg', id: '11'},
+                        {photo:'./images/mustache2.jpg', id: '12'},
+                        {photo:'./images/mustache3.jpg', id: '13'},
+                        {photo:'./images/mustache4.jpg', id: '14'},
+                        {photo:'./images/mustache5.jpg', id: '15'},
+                        {photo:'./images/mustache6.jpg', id: '16'},
+                        {photo:'./images/mustache7.jpg', id: '17'},
+                        {photo:'./images/mustache8.jpg', id: '18'},
+                        {photo:'./images/mustache9.jpg', id: '19'}
+                    ]
+                ;
+    }
+    // async getResult () {
+    //     try{
+    //         const res = await fetch('http://localhost:3000/persons');
+    //         const data = await res.json();
+    //         this.persons = data.persons;            
+    //     } catch (error) {
+    //         alert ('Something went wrong')
+    //     }
+    // }
+};
+
+export default Api;
