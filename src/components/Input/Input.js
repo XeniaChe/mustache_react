6e42f56d2dd4bb501d53a3671bcf46f12a0153cc
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-07-14 17:48:22 +0200

    Input error handling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,39 @@
-import React , {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import classes from './Input.module.scss';
 
 const Input = (props) => {
-    return(
-        <Fragment>
-            <div className={classes.form__group}>
-                <input type="text" className={classes.form__input} name={props.title}
-                    placeholder={props.placeholder} id={props.id} required onChange={props.changeInptut} ></input>
-                <label htmlFor={props.id} className={classes.form__label}>{props.title}</label>
-            </div>
-        </Fragment>
-    )
+	let style = [ classes.form__input ];
+
+	let validationError = null;
+	if (props.invalid && props.touched) {
+		style.push(classes.Invalid);
+
+		//Validation error handling
+		validationError = (
+			<p className={classes.ValidationError}>
+				Please, input the correct data
+			</p>
+		);
+	}
+	return (
+		<Fragment>
+			<div className={classes.form__group}>
+				<input
+					type='text'
+					className={style.join(' ')}
+					name={props.title}
+					placeholder={props.placeholder}
+					id={props.id}
+					required
+					onChange={props.changeInptut}
+				/>
+				<label htmlFor={props.id} className={classes.form__label}>
+					{props.title}
+				</label>
+			</div>
+			{validationError}
+		</Fragment>
+	);
 };
 
 export default Input;
