a0ab4bdcbf77361cc6859c0026749e098adae0ff
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-08-17 18:36:44 +0200

    08.17 logo src updated

--- src/components/Header/Header-logo/Header-logo.js
+++ src/components/Header/Header-logo/Header-logo.js
@@ -1,14 +1,19 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import classes from './Header-logo.module.scss';
 
 const Navigation = (props) => {
-    return (
-        <Fragment>
-                <div className={classes.header__logo}>
-                    <img src='./images/logo.jpg' alt="header logo" className={classes.logo_img}></img>
-                </div>
-        </Fragment>
-    )
+	const sourceImage = require(`../../../images/logo.jpg`);
+	return (
+		<Fragment>
+			<div className={classes.header__logo}>
+				<img
+					src={sourceImage}
+					alt='header logo'
+					className={classes.logo_img}
+				/>
+			</div>
+		</Fragment>
+	);
 };
 
 export default Navigation;
